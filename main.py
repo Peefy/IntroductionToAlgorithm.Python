97774e7dac50ff84f8f694f7548a58ffe3ba54bf
Peefy <xpf6677@163.com>
2018-01-31 14:42:33 +0800

    add fft test.
    add fft test.

--- main.py
+++ main.py
@@ -3,13 +3,23 @@
 
 import sys
 import numpy as nm
-import matplotlib.pyplot as mat
+import numpy.fft as fft
+import matplotlib.pyplot as plt
 
+## totol test nm.array plot fft
 X = nm.array([1, 2, 3, 4])
 Q = nm.array([1, 2, 3, 4])
+T = nm.arange(1, 100, 0.1)
+S = nm.sin(T)
 print(X)
-mat.plot(X, Q)
-mat.show()
+plt.plot(T, S)
+plt.figure()
+freq = fft.fftfreq(T.shape[-1])
+plt.plot(freq, fft.fft(S))
+plt.show()
+
+## Chapter.1
+
 
 # python main.py
 # python3 main.py
