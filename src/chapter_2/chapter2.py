e2675290aabe89bfbcaf1ed14c227f47033fe563
Peefy <xpf6677@163.com>
2018-02-05 18:32:49 +0800

    Finish 2.2 practices.
    Finish 2.2 practices.

--- src/chapter_2/chapter2.py
+++ src/chapter_2/chapter2.py
@@ -132,7 +132,7 @@ class Chapter2:
         '''
         index = None
         length = len(array)
-        for i in range(0, length):
+        for i in range(length):
             if v == array[i]:
                 index = i
         return index
@@ -167,9 +167,9 @@ class Chapter2:
         >>> range(0,3) 
         >>> [0, 1, 2]
         '''
-        returnSum = arange(0, length + 1)
+        returnSum = arange(length + 1)
         bitC = 0
-        for i in range(0, length):
+        for i in range(length):
             index = length - 1 - i
             bitSum = A[index] + B[index] + bitC
             if bitSum >= 2:
@@ -181,6 +181,37 @@ class Chapter2:
             if index == 0:
                 returnSum[0] = bitC
         return returnSum
+
+    def selectSortAscending(self, array = []):
+        '''
+        Summary
+        =
+        选择排序的升序排列
+        
+        Parameter
+        =
+        array : a list like
+        Return
+        =
+        sortedArray : 排序好的数组
+        >>> array = [1, 3, 5, 2, 4, 6]
+        >>> Chapter2().selectSortAscending(array)
+        >>> [1, 2, 3, 4, 5, 6]
+        '''
+        A = array
+        length = len(A)
+        for j in range(length):
+            minIndex = j
+            # 找出A中第j个到最后一个元素中的最小值
+            # 仅需要在头n-1个元素上运行
+            for i in range(j, length):
+                if A[i] <= A[minIndex]:
+                    minIndex = i
+            # 最小元素和最前面的元素交换
+            min = A[minIndex]
+            A[minIndex] = A[j]
+            A[j] = min
+        return A
 
     def note(self, *args, **kwargs):
         '''
@@ -201,6 +232,7 @@ class Chapter2:
         =
         >>> Chapter2().note()
         '''  
+        print('排序算法有很多，包括插入排序，冒泡排序，堆排序，归并排序，选择排序，计数排序，基数排序，桶排序，快速排序')
         print('2.1 插入排序')
         print('插入排序(INSERTION-SORT):输入n个数，输出n个数的升序或者降序排列')
         print('插入排序是一个对少量元素进行排序的有效算法，工作做原理与打牌摸牌整理手中的牌差不多')
@@ -228,21 +260,43 @@ class Chapter2:
         print('  21在序列A中的索引为(索引从0开始)：', self.arrayContains(A, 21))  
         print('练习2.1-4：两个n位二进制数相加的算法(适用于FPGA中。参考一位加法器的逻辑表达式或者数学表达式)：')
         print(' 两个n位二进制数的和为：', self.twoNBinNumAdd([1, 1, 0, 1], [1, 0, 0, 0]))
-        # range函数和arange函数都是左闭右开区间
+        # range函数和np.arange函数都是左闭右开区间
         print('range(0,4)的值为：', [i for i in range(0,4)])
         print('2.2 算法分析')
+        print('算法分析即指对一个算法所需要的资源进行预测')
+        print('内存，通信带宽或者计算机硬件等资源是关心的资源, 通常资源指我们希望测度的计算时间')
+        print('采用单处理器、随机存取机RAM计算模型')
+        print('RAM模型包含了真实计算机中常见的指令：算数指令(加法，减法，除法，取余，向下取整，向上取整指令)，数据移动指令(装入、存储、复制)和控制指令(条件和非条件转移、子程序调用和返回指令)')
+        print('RAM模型中的数据类型有整数类型和浮点实数类型')
+        print('算法分析所需要的数学工具包括组合数学、概率论、代数')      
+        print('需要对"运行时间"和"输入规模"更仔细地加以定义')
+        print('插入排序算法的分析')
+        print('插入排序INSERTION=SORT过程的时间开销与输入有关，排序1000个数的事件比排序三个数的时间要长')
+        print('插入算法即使对给定规模的输入，运行时间也有可能依赖于给定的是该规模下的哪种输入')
+        print('插入排序当输入是最好情况(即输入的序列已经按顺序排好)，插入排序所需要的时间随输入规模是线性的O(n)')
+        print('插入排序当输入是按照逆序排序的(降序排列输入后输出升序排列),就会出现最坏情况,所需要时间是输入规模的二次函数O(n^2)')
+        print('一般考察算法的最坏情况运行时间')
+        print('当然对于一些"随机化"算法，其行为即使对于固定的输入，运行时间也是可以变化的')
+        print('做进一步的抽象：即运行时间的增长率，只考虑算法运行时间公式中的最高次项，并且忽略最高次项的常数系数,时间复杂度')
+        print('练习题2.2-1: n^3/1000 - 100n^2 - 100n + 3 的时间复杂度：O(n^3)')
+        print('练习题2.2-2:选择排序如下：')
+        A = [21, 11, 9, 66, 51, 48]
+        print(' 选择排序排列前的元素：', A)
+        print(' 选择排序排列后的元素：', self.selectSortAscending(A))
+        print(' 选择排序最好情况o(n^2),最坏情况o(n^2)')
+        print(' 因为在第n-1次比较选择的时候已经比较出了最大元素和次大元素并选择，所以这时选择完之后第n个元素已经是最大值，没有必要再比较下去了')
+        print('练习题2.2-3:线性查找最好情况是o(1),最坏情况是o(n)，平均情况是(n)')
+        print('要使算法具有较好的最佳情况运行时间就一定要对输入进行控制，使之偏向能够使得算法具有最佳运行情况的排列。')
         #python src/chapter_2/chapter2.py
         #python3 src/chapter_2/chapter2.py
-        print()
         return self
 
 if __name__ == '__main__':
-    print('single chapter two!')
+    print('Run main : single chapter two!')
     Chapter2().note()
-    print('')
-
 else:
-    print('please in your cmd input as follow:\n python src/chapter_1/chapter1.py or \n' + 
+    print('please in your cmd input as follow:\n' + 
+        'python src/chapter_2/chapter2.py or \n' + 
         'python3 src/chapter_1/chapter1.py')
     print()
 
