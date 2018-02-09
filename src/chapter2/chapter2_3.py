3a6f0e723f9421beaee8c914f27deb4e602f7f40
Peefy <xpf6677@163.com>
2018-02-09 20:40:26 +0800

    Add comments.
    Add comments.

--- src/chapter2/chapter2_3.py
+++ src/chapter2/chapter2_3.py
@@ -167,6 +167,7 @@ class Chapter2_3:
         sortedArray : 排序好的数组
 
         Example:
+        =
         >>> Chapter2_3().mergeSort([6, 5, 4, 3, 2, 1])
         >>> [1, 2, 3, 4, 5, 6]
         '''
@@ -201,6 +202,7 @@ class Chapter2_3:
         sortedArray : 排序好的数组
 
         Example:
+        =
         >>> Chapter2_3().mergeSort([6, 5, 4, 3, 2, 1])
         >>> [1, 2, 3, 4, 5, 6]
 
