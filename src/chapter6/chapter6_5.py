566561821cac58bc1cdb48b2e85379f94668a819
Peefy <xpf6677@163.com>
2018-03-08 22:29:49 +0800

    Start chapter6 task
    Start chapter6 task

--- src/chapter6/chapter6_5.py
+++ src/chapter6/chapter6_5.py
@@ -36,7 +36,32 @@ class Chapter6_5:
         print('和堆一样，队列也有两种，最大优先级队列和最小优先级队列')
         print('优先级队列是一种用来维护由一组元素构成的集合S的数据结构，这一组元素中的每一个都有一个关键字key')
         print('一个最大优先级队列支持一下操作')
-        print('INSERT(S, x):把元素x插入集合S，这一操作可写为S<-S')
+        print('INSERT(S, x):把元素x插入集合S，这一操作可写为S<-S∪{x}')
+        print('MAXIMUM(S)：返回S中具有最大关键字')
+        print('EXTRACT-MAX(S)：去掉并返回S中的具有最大关键字的元素')
+        print('INCREASE-KEY(S，x, k):将元素x的关键字的值增加到k，这里k值不能小于x的原关键字的值')
+        print('最大优先级队列的一个应用是在一台分时计算机上进行作业调度。')
+        print('当一个作业做完或被中断时，用EXTRACT-MAX操作从所有等待的作业中，选择出具有最高优先级的作业')
+        print('在任何时候，一个新作业都可以用INSERT加入到队列中去')
+        print('当用堆来实现优先级队列时，需要在堆中的每个元素里存储对应的应用对象的柄handle，',
+            '对象柄的准确表示到底怎样(一个指针或者一个整形数)还取决于具体的应用')
+        A = [14, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1]
+        print('练习6.5-1 数组A=', _deepcopy(A), '执行HEAP-EXTRACT-MAX操作的过程为：', heap.extractmax(A), A)
+        A = [15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1]
+        print(' 数组A=', _deepcopy(A), '执行HEAP-INCREASE-KEY操作的过程为：', heap.increasekey(A, 2, 16))
+        A = [15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1]
+        print('练习6.5-2 数组A=', _deepcopy(A), '执行MAX-HEAP-INSERT(A, 10)操作的过程为：', heap.maxheapinsert(A, 10))
+        print('练习6.5-3 基本把最大堆算法的不等号方向改以以下就可以')
+        print('练习6.5-4 因为插入的元素并不知道其大小和插入前原始最大堆元素的大小比较情况，所以将插入的数据放到二叉堆的最底部的叶子上而且是最小值(负无穷)')
+        print('练习6.5-5 略')
+        print('练习6.5-6 先进先出队列和栈')
+        A = [15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1]
+        print('练习6.5-7 数组A=', _deepcopy(A), '执行HEAP-DELETE(A, 2)操作的过程为：', heap.maxheapdelete(A, 2))
+        print('练习6.5-8 略(不会)')
+        print('思考题6-1 用插入的方法建堆')
+        print('思考题6-2 对d叉堆的分析')
+        print('思考题6-3 Young氏矩阵')
+        print('')
         # python src/chapter6/chapter6_5.py
         # python3 src/chapter6/chapter6_5.py
         return self
--- src/chapter6/heap.py
+++ src/chapter6/heap.py
@@ -2,6 +2,9 @@
 :二叉堆:的一系列操作
 '''
 
+import math as _math
+from numpy import arange as _arange
+
 def left(i):
     '''
     求:二叉堆:一个下标i的:左儿子:的下标
@@ -152,6 +155,113 @@ def heapsort(A):
         __maxheapify(A, 0)
     return A
         
-    
+def extractmax(A):
+    '''
+    去掉集合A中具有最大关键字的元素重新构建最大堆,运行时间为:O(lgn):
 
+    Args
+    =
+    A : 待去掉最大元素的集合A
 
+    Return：
+    =
+    max : 去掉的最大元素
+
+    '''
+    heapsizeA = heapsize(A)
+    if heapsizeA < 1:
+        raise Exception('heap underflow')
+    max = A[0]
+    A[0] = A[heapsizeA]
+    heapsizeA = heapsizeA - 1
+    maxheapify(A, 0)
+    return max
+
+def increasekey(A, i, key):
+    '''
+    将索引为`i`的关键字的值加到`key`，这里`key`的值不能小于索引为`i`原关键字的值并重新构建:最大堆:
+
+    Args
+    =
+    A : 待操作的集合A
+    i : 索引
+    key : 提升后的值
+
+    Return
+    =
+    A : 操作完成的集合A
+
+    Example
+    ==
+    >>> import heap
+    >>> heap.increasekey([4,3,2,1],1,5)
+    >>> [5,3,2,1]
+    >>> heap.increasekey([4,3,2,1],2,5)
+    >>> [5,4,3,1]
+
+    '''
+    if key < A[i]:
+        raise Exception('new key is smaller than current key')
+    A[i] = key
+    # 构建最大堆
+    while i > 0 and A[parent(i)] < A[i]:
+        A[i], A[parent(i)] = A[parent(i)], A[i]
+        i = parent(i)
+    return A
+
+def maxheapinsert(A, key):
+    '''
+    向最大堆中插入一个值为`key`的元素，并重新构成:最大堆:
+
+    Args
+    =
+    A : 待插入元素的数组
+    key : 待插入元素的值
+
+    Return
+    ==
+    A : 插入完成的元素
+
+    '''
+    heapsizeA = heapsize(A) + 1
+    A.append(-_math.inf)
+    increasekey(A, heapsizeA, key)
+    return A
+
+def maxheapdelete(A, i):
+    '''
+    删除一个最大堆索引为`i`的元素：运行代价为:O(nlgn):
+
+    Args
+    =
+    A : 待操作的数组A
+    i : 待删除的索引i
+
+    Return
+    =
+    A : 删除操作完成后的元素
+
+    Example
+    =
+    >>> import heap
+    >>> heap.maxheapdelete([4,3,2,1],0)
+    >>> [3,2,1]
+
+    See Also
+    =
+    >>> heap.maxheapinsert
+
+    '''
+    heapsizeA = heapsize(A) - 1
+    count = len(A)
+    if i >= count:
+        raise Exception('the arg i must not i >= len(A)!')
+    A[i] = -_math.inf
+    maxheapify(A, i)
+    A.pop()
+    return A
+
+def buildmaxheap_usesort(A):
+    '''
+    将数组A构建成为一个:最大堆:
+    '''
