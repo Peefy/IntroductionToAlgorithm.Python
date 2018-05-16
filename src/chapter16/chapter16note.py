c6a46abd3022442de3eccd7f895d7a246430c7c5
Peefy <xpf6677@163.com>
2018-05-16 22:01:17 +0800

    add some

--- src/chapter16/chapter16note.py
+++ src/chapter16/chapter16note.py
@@ -5,6 +5,7 @@
 '''
 Class Chapter16_1
 
+Class Chapter16_2
 
 '''
 from __future__ import absolute_import, division, print_function
@@ -39,10 +40,51 @@ class Chapter16_1:
         '''
         print('chapter16.1 note as follow')   
         print('第16章 贪心算法')
+        # !适用于最优化问题的算法往往具有一系列步骤，每一步有一组选择
+        print('适用于最优化问题的算法往往具有一系列步骤，每一步有一组选择')
+        print('对许多最优化的问题来说，采用动态规划有点大材小用的意思，只要采用另一些更简单的方法就行了')
+        # !贪心算法使得所做的选择在当前看起来都是最佳的，期望通过所做的局部最优得到最终的全局最优
+        print('贪心算法使得所做的选择在当前看起来都是最佳的，期望通过所做的局部最优得到最终的全局最优')
+        print('贪心算法对大多数最优化问题都能产生最优解，但也不一定总是这样的')
+        print('在讨论贪心算法前，首先讨论动态规划方法，然后证明总能用贪心的选择得到其最优解')
+        print('16.2给出一种证明贪心算法正确的方法')
+        # !有许多被视为贪心算法应用的算法，如最小生成树，Dijkstra的单源最短路径，贪心集合覆盖启发式
+        print('有许多被视为贪心算法应用的算法，如最小生成树，Dijkstra的单源最短路径，贪心集合覆盖启发式')
+        print('16.1 活动选择问题')
+        # !活动选择问题：对几个互相竞争的活动进行调度,它们都要求以独占的方式使用某一公共资源
+        print('对几个互相竞争的活动进行调度,它们都要求以独占的方式使用某一公共资源')
+        print('设有n个活动和某一个单独资源构成的集合S={a1,a2,...,an}，该资源一次只能被一个活动占用')
+        print('每个活动ai都有一个开始时间si和一个结束时间fi，资源一旦被活动ai选中后')
+        print('活动ai就开始占据左闭右开时间区间，如果两个活动ai，aj的时间没有交集，称ai和aj是兼容的')
+        print('')
+        print('')
+        print('')
+        print('')
+        # python src/chapter16/chapter16note.py
+        # python3 src/chapter16/chapter16note.py
+
+class Chapter16_2:
+    '''
+    chpater16.2 note and function
+    '''
+    def note(self):
+        '''
+        Summary
+        ====
+        Print chapter16.2 note
+
+        Example
+        ====
+        ```python
+        Chapter16_2().note()
+        ```
+        '''
+        print('chapter16.2 note as follow')   
         # python src/chapter16/chapter16note.py
         # python3 src/chapter16/chapter16note.py
 
 chapter16_1 = Chapter16_1()
+chapter16_2 = Chapter16_2()
 
 def printchapter16note():
     '''
@@ -50,7 +92,7 @@ def printchapter16note():
     '''
     print('Run main : single chapter sixteen!')  
     chapter16_1.note()
-
+    chapter16_2.note()
 
 # python src/chapter16/chapter16note.py
 # python3 src/chapter16/chapter16note.py
