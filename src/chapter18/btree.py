3afa8d6ae8243c26b6ff5068e18abe238e6dc403
Peefy <xpf6677@163.com>
2018-06-10 16:19:50 +0800

    Add some

--- src/chapter18/btree.py
+++ src/chapter18/btree.py
@@ -30,8 +30,8 @@ class BTreeNode:
         '''
         pass
 
-    @staticmethod
-    def allocate_node():
+    @classmethod
+    def allocate_node(self):
         '''
         在O(1)时间内为一个新结点分配一个磁盘页
 
@@ -60,6 +60,7 @@ class BTree:
         self.lastnode: BTreeNode = None
         self.root: BTreeNode = None
         self.nodes = []
+        self.t = 4
 
     @staticmethod
     def create():
@@ -116,3 +117,51 @@ class BTree:
             # 递归搜索x的适当子树
             return self.search(x.children[i], k)
 
+    def split_child(self, x : BTreeNode, i, y : BTreeNode):
+        '''
+        B树分裂
+
+        满结点`y`按其中间关键字S的索引`i``进行分裂，S则提升到`y`的双亲结点`x`当中
+
+        Args
+        ===
+        `x` : `y`的双亲结点
+
+        `y` : 要分裂的结点
+
+        `i` : 分裂结点`y`的分裂索引`i`
+        '''
+        t = self.t
+        z = BTreeNode.allocate_node()
+        z.leaf = y.leaf
+        z.n = t - 1
+        for j in range(t - 1):
+            z.keys[j] = z.keys[j + t]
+        if not y.leaf:
+            for j i in range(t):
+                z.children[j] = z.children[j + t]
+        y.n = t - 1
+        for j in range(i + 1, x.n + 1):
+            k = x.n + 1 - j + i + 1
+            x.children[k + 1] = x.children[k]
+        x.children[i + 1] = z
+        for j in range(i, x.n):
+            k = x.n - j + i
+            x.keys[k + 1] = x.keys[k]
+        x.keys[i] = y.keys[t]
+        x.n = x.n + 1
+        y.diskwrite()
+        z.diskwrite()
+        x.diskwrite()
+
+def test():
+    '''
+    test class `BTree` and class `BTreeNode`
+    '''
+    tree = BTree.create()
+    print(tree.root)
+
+if __name__ == '__main__':
+    test()
+else:
+    pass
