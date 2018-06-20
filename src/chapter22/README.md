cf4c53e3a15481a831c9fd0f29d1fa95708d2465
Peefy <xpf6677@163.com>
2018-06-20 17:20:31 +0800

    Add some

--- src/chapter22/README.md
+++ src/chapter22/README.md
@@ -10,4 +10,183 @@
 这两种表示法即可以用于有向图，也可以用于无向图
 通常采用邻接表表示法，因为用这种方法表示稀疏图比较紧凑
 
+## 广度优先搜索
+
+广度优先搜素是最简单的图搜索算法之一,也是很多重要的图算法的原型
+
+```python
+
+import math as _math
+
+from numpy import *
+
+class Graph:
+    '''
+    图`G=(V,E)`
+    '''
+    def __init__(self, vertexs : list = None, edges : list = None):
+        '''
+        图`G=(V,E)`
+
+        Args
+        ===
+        `vertexs` : 图的顶点
+
+        `edges` : 图的边
+
+        '''
+        self.veterxs = vertexs
+        self.edges = edges
+   
+    def getadj(self):
+        '''
+        获取邻接表
+        '''
+        adj = []
+        n = len(self.veterxs)
+        if n == 0:
+            return []
+        for i in range(n):    
+            sub = []     
+            for edge in self.edges:
+                dir = ' '
+                if len(edge) == 2:
+                    u, v = edge
+                else:
+                    u, v, dir = edge
+                uindex = self.veterxs.index(u)
+                vindex = self.veterxs.index(v)
+                if dir == '→' and uindex == i:
+                    sub.append(v)
+                elif uindex == i:
+                    sub.append(v)
+            adj.append(sub)
+        return adj
+
+    def getmatrix(self):
+        '''
+        获取邻接矩阵,并且其是一个对称矩阵
+        '''
+        n = len(self.veterxs)
+        if n == 0:
+            return []
+        mat = zeros((n, n))
+        for edge in self.edges:
+            dir = ' '
+            if len(edge) == 2:
+                u, v = edge
+            else:
+                u, v, dir = edge
+            uindex = self.veterxs.index(u)
+            vindex = self.veterxs.index(v)                         
+            if dir == '→':
+                mat[uindex, vindex] = 1
+            elif dir == '←':
+                mat[vindex, uindex] = 1
+            else:
+                mat[uindex, vindex] = 1
+                mat[vindex, uindex] = 1
+        return mat
+
+    def __get_rev_dir(self, dir):
+        if dir == '→':
+            dir = '←'
+        elif dir == '←':
+            dir = '→'
+        else:
+            dir = ' '
+        return dir
+
+    def buildrevedges(self):
+        '''
+        构造反向的有向图边
+        '''
+        newedges = []
+        n = len(self.edges)
+        for i in range(n):
+            edge = self.edges[i]
+            v1, v2, dir = edge
+            edge_rev = v2, v1, self.__get_rev_dir(dir)
+            newedges.append(edge_rev)
+        return newedges
+
+    def buildBMatrix(self):
+        '''
+        构造关联矩阵
+        '''
+        m = len(self.veterxs)
+        n = len(self.edges)
+        B = zeros((m, n))
+        revedges = self.buildrevedges()
+        for i in range(m):
+            v = self.veterxs[i]
+            for j in range(n):
+                v1, v2, dir = self.edges[j]
+                if v1 != v and v2 != v:
+                    B[i][j] = 0
+                elif v1 == v and dir == '→':
+                    B[i][j] = -1
+                elif v2 == v and dir == '←':
+                    B[i][j] = -1
+                elif v1 == v and dir == '←':
+                    B[i][j] = 1
+                elif v2 == v and dir == '→ ':
+                    B[i][j] = 1
+            for j in range(n):
+                v1, v2, dir = revedges[j]
+                if v1 != v and v2 != v:
+                    B[i][j] = 0
+                elif v1 == v and dir == '→':
+                    B[i][j] = -1
+                elif v2 == v and dir == '←':
+                    B[i][j] = -1
+                elif v1 == v and dir == '←':
+                    B[i][j] = 1
+                elif v2 == v and dir == '→ ':
+                    B[i][j] = 1
+        return matrix(B)
+    
+    def contains_uni_link(self):
+        '''
+        确定有向图`G=(V,E)`是否包含一个通用的汇(入度为|V|-1,出度为0的点)
+        '''
+        return False
+
+def undirected_graph_test():
+    g = Graph()
+    g.veterxs = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    g.edges = [('a', 'b'), ('a', 'c'), ('b', 'd'),
+               ('b', 'e'), ('c', 'f'), ('c', 'g')]
+    print('邻接表为')
+    print(g.getadj())
+    print('邻接矩阵为')
+    print(g.getmatrix())
+
+def directed_graph_test():
+    g = Graph()
+    g.veterxs = ['1', '2', '3', '4', '5', '6']
+    g.edges = [('1', '2', '→'), ('4', '2', '→'), 
+               ('1', '4', '→'), ('2', '5', '→'),
+               ('3', '6', '→'), ('3', '5', '→'),
+               ('5', '4', '→'), ('6', '6', '→')]
+    print('邻接表为')
+    print(g.getadj())
+    print('邻接矩阵为')
+    print(g.getmatrix())
+    B = g.buildBMatrix()
+    print('关联矩阵为')
+    print(B)
+    print(B * B.T)
+    print('是否包含通用的汇', g.contains_uni_link())
+
+def test():
+    undirected_graph_test()
+    directed_graph_test()
+
+if __name__ == '__main__':
+    test()
+else:
+    pass
+```
+
 [Github Code](https://github.com/Peefy/CLRS_dugu_code-master/blob/master/src/chapter22)
--- src/chapter22/graph.py
+++ src/chapter22/graph.py
@@ -1,13 +1,76 @@
 
 import math as _math
 
-from numpy import *
+import numpy as _np
+
+COLOR_WHITE = 0
+COLOR_GRAY = 1
+COLOR_BLACK = 2
+
+DIRECTION_NONE = ' '
+DIRECTION_TO = '→'
+DIRECTION_FROM = '←'
+DIRECTION_BOTH = '←→'
+
+class Vertex:
+    '''
+    图的顶点
+    '''
+    def __init__(self, key = None):
+        '''
+        图的顶点
+
+        Args
+        ===
+        `key` : 顶点关键字
+
+        '''
+        self.key = key
+        self.color = COLOR_WHITE
+        self.d = _math.inf
+        self.pi = None
+
+    def __str__(self):
+        return '[key:{} color:{} d:{} pi:{}]'.format(self.key, self.color, self.d, self.pi)
+
+class Edge:
+    '''
+    图的边，包含两个顶点
+    '''
+    def __init__(self, vertex1 : Vertex = None, \
+            vertex2 : Vertex = None, \
+            distance = 1, \
+            dir = DIRECTION_NONE,
+            ):
+        '''
+        图的边，包含两个顶点
+
+        Args
+        ===
+        `vertex1` : 边的一个顶点
+        
+        `vertex2` : 边的另一个顶点
+
+        `dir` : 边的方向   
+            DIRECTION_NONE : 没有方向
+            DIRECTION_TO : `vertex1` → `vertex2`
+            DIRECTION_FROM : `vertex1` ← `vertex2`
+            DIRECTION_BOTH : `vertex1` ←→ `vertex2`
+
+        '''
+        self.dir = dir
+        self.vertex1 = vertex1
+        self.vertex2 = vertex2
+        self.distance = distance
+
+    def __str__(self):
+        return str((self.vertex1, self.vertex2, self.dir))
 
 class Graph:
     '''
     图`G=(V,E)`
     '''
-    def __init__(self, vertexs : list = None, edges : list = None):
+    def __init__(self, vertexs : list = [], edges : list = []):
         '''
         图`G=(V,E)`
 
@@ -21,6 +84,39 @@ class Graph:
         self.veterxs = vertexs
         self.edges = edges
         self.adj = []
+        self.matrix = []
+   
+    def getadj(self):
+        '''
+        获取邻接表
+        '''
+        adj = []
+        n = len(self.veterxs)
+        if n == 0:
+            return []
+        for i in range(n):    
+            sub = []     
+            for edge in self.edges:
+                dir = ' '
+                if type(edge) is Edge:
+                    u, v, dir = edge.vertex1, edge.vertex2, edge.dir
+                elif len(edge) == 2:
+                    u, v = edge
+                else:
+                    u, v, dir = edge
+                uindex = self.veterxs.index(u)
+                vindex = self.veterxs.index(v)
+                if dir == DIRECTION_TO and uindex == i:
+                    sub.append(v)
+                elif dir == DIRECTION_FROM and vindex == i:
+                    sub.append(u)
+                elif dir == DIRECTION_NONE and uindex == i:
+                    sub.append(v)
+                elif dir == DIRECTION_NONE and vindex == i:
+                    sub.append(u)               
+            adj.append(sub)
+        self.adj = adj
+        return adj
 
     def getmatrix(self):
         '''
@@ -29,21 +125,241 @@ class Graph:
         n = len(self.veterxs)
         if n == 0:
             return []
-        mat = zeros((n, n))
+        mat = _np.zeros((n, n))
         for edge in self.edges:
-            u, v = edge
+            dir = ' '
+            if type(edge) is Edge:
+                u, v, dir = edge.vertex1, edge.vertex2, edge.dir 
+            elif len(edge) == 2:
+                u, v = edge
+            else:
+                u, v, dir = edge
             uindex = self.veterxs.index(u)
-            vindex = self.veterxs.index(v)
-            mat[uindex, vindex] = 1
-            mat[vindex, uindex] = 1
+            vindex = self.veterxs.index(v)                         
+            if dir == DIRECTION_TO:
+                mat[uindex, vindex] = 1
+            elif dir == DIRECTION_FROM:
+                mat[vindex, uindex] = 1
+            else:
+                mat[uindex, vindex] = 1
+                mat[vindex, uindex] = 1
+        self.matrix = mat
         return mat
 
-def test():
+    def __get_rev_dir(self, dir):
+        if dir == DIRECTION_FROM:
+            dir = DIRECTION_TO
+        elif dir == DIRECTION_TO:
+            dir = DIRECTION_FROM
+        else:
+            dir = DIRECTION_NONE
+        return dir
+
+    def buildrevedges(self):
+        '''
+        构造反向的有向图边
+        '''
+        newedges = []
+        n = len(self.edges)
+        for i in range(n):
+            edge = self.edges[i]
+            if type(edge) is Edge:
+                v1, v2, dir = edge.vertex1, edge.vertex2, edge.dir
+            else:
+                v1, v2, dir = edge
+            edge_rev = v2, v1, self.__get_rev_dir(dir)
+            newedges.append(edge_rev)
+        return newedges
+
+    def __buildBMatrix(self, B, v, i, j, v1, v2, dir):
+        if v1 != v and v2 != v:
+            B[i][j] = 0
+        elif v1 == v and dir == DIRECTION_TO:
+            B[i][j] = -1
+        elif v2 == v and dir == DIRECTION_FROM:
+            B[i][j] = -1
+        elif v1 == v and dir == DIRECTION_FROM:
+            B[i][j] = 1
+        elif v2 == v and dir == DIRECTION_TO:
+            B[i][j] = 1
+
+    def buildBMatrix(self):
+        '''
+        构造关联矩阵
+        '''
+        m = len(self.veterxs)
+        n = len(self.edges)
+        B = _np.zeros((m, n))
+        revedges = self.buildrevedges()
+        for i in range(m):
+            v = self.veterxs[i]
+            for j in range(n):
+                edge = self.edges[j]
+                if type(edge) is Edge:
+                    v1, v2, dir = edge.vertex1, edge.vertex2, edge.dir
+                else:
+                    v1, v2, dir = edge
+                self.__buildBMatrix(B, v, i, j, v1, v2, dir)
+            for j in range(n):
+                v1, v2, dir = revedges[j]
+                self.__buildBMatrix(B, v, i, j, v1, v2, dir)
+        return _np.matrix(B)
+    
+    def contains_uni_link(self):
+        '''
+        确定有向图`G=(V,E)`是否包含一个通用的汇(入度为|V|-1,出度为0的点)
+        '''
+        n = len(self.veterxs)
+        self.getmatrix()
+        m = self.matrix
+        for i in range(n):
+            if sum(m[i]) == n - 1:
+                return True
+        return False
+
+def bfs(g : Graph, s : Vertex):
+    '''
+    广度优先搜索(breadth-first search)
+
+    Args
+    ===
+    `g` : type:`Graph`,图`G(V,E)`(无向图或者有向图均可)
+
+    `s` : type:`Vertex`，搜索的起点顶点
+
+    Return
+    ===
+    None
+
+    Example
+    ===
+    ```python
+    from graph import *
+    g = Graph()
+    v = [Vertex('a'), Vertex('b'), Vertex('c'), Vertex('d'), Vertex('e')]
+    g.veterxs = v
+    g.edges.append(Edge(v[0], v[1]))
+    g.edges.append(Edge(v[0], v[2]))
+    g.edges.append(Edge(v[1], v[3]))
+    g.edges.append(Edge(v[2], v[1]))
+    g.edges.append(Edge(v[3], v[0]))
+    g.edges.append(Edge(v[4], v[3]))
+    print('邻接表为')
+    print(g.getadj())
+    print('邻接矩阵为')
+    print(g.getmatrix())
+    for i in range(len(v)):
+        bfs(g, v[i])
+        print('{}到各点的距离为'.format(v[i]))
+        for u in g.veterxs:
+            print(u.d, end=' ')
+        print(' ')
+    ```
+
+    '''
+    adj = g.getadj()
+    n = len(g.veterxs)
+    for i in range(n):
+        u = g.veterxs[i]
+        if type(u) is Vertex:
+            u.color = COLOR_WHITE
+            u.d = _math.inf
+            u.pi = None
+        else:
+            return
+    s.color = COLOR_GRAY
+    s.d = 0
+    s.pi = None
+    q = []
+    q.append(s)
+    while len(q) != 0:
+        u = q.pop(0)
+        uindex = g.veterxs.index(u)
+        for i in range(len(adj[uindex])):
+            v = adj[uindex][i]
+            if v.color == COLOR_WHITE:
+                v.color = COLOR_GRAY
+                v.d = u.d + 1
+                v.pi = u
+                q.append(v)
+        u.color = COLOR_BLACK
+    
+def undirected_graph_test():
     g = Graph()
     g.veterxs = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     g.edges = [('a', 'b'), ('a', 'c'), ('b', 'd'),
                ('b', 'e'), ('c', 'f'), ('c', 'g')]
+    print('邻接表为')
+    print(g.getadj())
+    print('邻接矩阵为')
     print(g.getmatrix())
+
+def directed_graph_test():
+    g = Graph()
+    g.veterxs = ['1', '2', '3', '4', '5', '6']
+    g.edges = [('1', '2', '→'), ('4', '2', '→'), 
+               ('1', '4', '→'), ('2', '5', '→'),
+               ('3', '6', '→'), ('3', '5', '→'),
+               ('5', '4', '→'), ('6', '6', '→')]
+    print('邻接表为')
+    print(g.getadj())
+    print('邻接矩阵为')
+    print(g.getmatrix())
+    B = g.buildBMatrix()
+    print('关联矩阵为')
+    print(B)
+    print(B * B.T)
+    print('是否包含通用的汇', g.contains_uni_link())
+
+def test_bfs():
+    g = Graph()
+    v = [Vertex('a'), Vertex('b'), Vertex('c'), Vertex('d'), Vertex('e')]
+    g.veterxs = v
+    g.edges.append(Edge(v[0], v[1]))
+    g.edges.append(Edge(v[0], v[2]))
+    g.edges.append(Edge(v[1], v[3]))
+    g.edges.append(Edge(v[2], v[1]))
+    g.edges.append(Edge(v[3], v[0]))
+    g.edges.append(Edge(v[4], v[3]))
+    print('邻接表为')
+    print(g.getadj())
+    print('邻接矩阵为')
+    print(g.getmatrix())
+    for i in range(len(v)):
+        bfs(g, v[i])
+        print('{}到各点的距离为'.format(v[i]))
+        for u in g.veterxs:
+            print(u.d, end=' ')
+        print(' ')
+    del g
+
+    gwithdir = Graph()
+    vwithdir = [Vertex('a'), Vertex('b'), Vertex('c'),
+                Vertex('d'), Vertex('e')]
+    gwithdir.veterxs = vwithdir
+    gwithdir.edges.clear()
+    gwithdir.edges.append(Edge(vwithdir[0], vwithdir[1], 1, DIRECTION_TO))
+    gwithdir.edges.append(Edge(vwithdir[0], vwithdir[2], 1, DIRECTION_TO))
+    gwithdir.edges.append(Edge(vwithdir[1], vwithdir[3], 1, DIRECTION_TO))
+    gwithdir.edges.append(Edge(vwithdir[2], vwithdir[1], 1, DIRECTION_TO))
+    gwithdir.edges.append(Edge(vwithdir[3], vwithdir[0], 1, DIRECTION_TO))
+    gwithdir.edges.append(Edge(vwithdir[4], vwithdir[3], 1, DIRECTION_TO))
+    print('邻接表为')
+    print(gwithdir.getadj())
+    print('邻接矩阵为')
+    print(gwithdir.getmatrix())
+    for i in range(len(vwithdir)):
+        bfs(gwithdir, vwithdir[i])
+        print('{}到各点的距离为'.format(vwithdir[i]))
+        for u in gwithdir.veterxs:
+            print(u.d, end=' ')
+        print('')
+    del gwithdir
+
+def test():
+    undirected_graph_test()
+    directed_graph_test()
+    test_bfs()
 
 if __name__ == '__main__':
     test()
