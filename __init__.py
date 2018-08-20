1d28fee670455ad11916a5d6db14225e75a42f6f
Peefy <xpf6677@163.com>
2018-08-20 20:10:00 +0800

    Add chapter29 folder

--- __init__.py
+++ __init__.py
@@ -5,5 +5,5 @@
 # python main.py
 # python3 main.py
 
-# pdf.463
+# pdf.481
 
---
+++ src/chapter29/chapter29note.py
@@ -0,0 +1,160 @@
+# coding:utf-8
+# usr/bin/python3
+# python src/chapter29/chapter29note.py
+# python3 src/chapter29/chapter29note.py
+"""
+
+Class Chapter29_1
+
+Class Chapter29_2
+
+Class Chapter29_3
+
+Class Chapter29_4
+
+Class Chapter29_5
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+
+class Chapter29_1:
+    """
+    chapter29.1 note and function
+    """
+    def __init__(self):
+        pass
+
+    def note(self):
+        """
+        Summary
+        ====
+        Print chapter29.1 note
+
+        Example
+        ====
+        ```python
+        Chapter29_1().note()
+        ```
+        """
+        print('chapter29.1 note as follow')
+        # python src/chapter29/chapter29note.py
+        # python3 src/chapter29/chapter29note.py
+
+class Chapter29_2:
+    """
+    chapter29.2 note and function
+    """
+    def __init__(self):
+        pass
+
+    def note(self):
+        """
+        Summary
+        ====
+        Print chapter29.2 note
+
+        Example
+        ====
+        ```python
+        Chapter29_2().note()
+        ```
+        """
+        print('chapter29.2 note as follow')
+        # python src/chapter29/chapter29note.py
+        # python3 src/chapter29/chapter29note.py
+
+class Chapter29_3:
+    """
+    chapter29.3 note and function
+    """
+    def __init__(self):
+        pass
+
+    def note(self):
+        """
+        Summary
+        ====
+        Print chapter29.3 note
+
+        Example
+        ====
+        ```python
+        Chapter29_3().note()
+        ```
+        """
+        print('chapter29.3 note as follow')
+        # python src/chapter29/chapter29note.py
+        # python3 src/chapter29/chapter29note.py
+
+class Chapter29_4:
+    """
+    chapter29.4 note and function
+    """
+    def __init__(self):
+        pass
+
+    def note(self):
+        """
+        Summary
+        ====
+        Print chapter29.4 note
+
+        Example
+        ====
+        ```python
+        Chapter29_4().note()
+        ```
+        """
+        print('chapter29.4 note as follow')
+        # python src/chapter29/chapter29note.py
+        # python3 src/chapter29/chapter29note.py
+
+class Chapter29_5:
+    """
+    chapter29.5 note and function
+    """
+    def __init__(self):
+        pass
+
+    def note(self):
+        """
+        Summary
+        ====
+        Print chapter29.5 note
+
+        Example
+        ====
+        ```python
+        Chapter29_5().note()
+        ```
+        """
+        print('chapter29.5 note as follow')
+        # python src/chapter29/chapter29note.py
+        # python3 src/chapter29/chapter29note.py
+
+chapter29_1 = Chapter29_1()
+chapter29_2 = Chapter29_2()
+chapter29_3 = Chapter29_3()
+chapter29_4 = Chapter29_4()
+chapter29_5 = Chapter29_5()
+
+def printchapter29note():
+    """
+    print chapter29 note.
+    """
+    print('Run main : single chapter twenty-nine!')
+    chapter29_1.note()
+    chapter29_2.note()
+    chapter29_3.note()
+    chapter29_4.note()
+    chapter29_5.note()
+
+# python src/chapter29/chapter29note.py
+# python3 src/chapter29/chapter29note.py
+
+if __name__ == '__main__':  
+    printchapter29note()
+else:
+    pass
