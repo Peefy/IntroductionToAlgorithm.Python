c4fc693a32b690b0ac635edb600c846c7a7c77bb
Peefy <xpf6677@163.com>
2018-09-17 22:09:21 +0800

    Add some

--- src/chapter33/chapter33note.py
+++ src/chapter33/chapter33note.py
@@ -40,6 +40,22 @@ class Chapter33_1:
         ```
         """
         print('chapter33.1 note as follow')
+        print('第33章 计算几何学')
+        print('计算几何学问题的输入一般是关于一组几何对象的描述,如一组点、一组线段,或者一个多边形的按逆时针顺序排列的一组顶点')
+        print('输出常常是对有关这些对象的问题的回答,如是否直线相交,是否为一个新的几何对象,如顶点集合的凸包(convex hull,即最小封闭多边形)')
+        print('本章中,将学习一些二维的(即平面上的)计算几何学算法,在这些算法中,每个输入对象都用一组点{p1,p2,p3,...}来表示,其中每个pi=(xi,yi)',
+            'xi,yi∈R.例如：一个顶点的多边形P可以用一组点<p0,p1,p2,...,pn-1>来表示,这些点按照在P的边界上出现的顺序排列.')
+        print('计算几何学也可以用来求解三维空间,甚至高维空间中的问题,但这样的问题及其解决方案是很难视觉化的')
+        print('不过,即使是在二维平面上,也能够看到应用计算几何学技术的一些很好的例子')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
         # python src/chapter33/chapter33note.py
         # python3 src/chapter33/chapter33note.py
 
