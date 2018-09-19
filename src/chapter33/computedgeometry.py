9784804d170ea5403b5f17bb079b9b531d6484c0
Peefy <xpf6677@163.com>
2018-09-19 14:27:45 +0800

    Add some

---
+++ src/chapter33/computedgeometry.py
@@ -0,0 +1,28 @@
+
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+class _ComputedGeometry:
+    def __init__(self):
+        pass
+
+    def direction(self, pi, pj, pk):
+        pass
+
+    def segments_intersect(self, p1, p2, p3, p4):
+        """
+        """
+        pass
+
+def test():
+    """
+    测试函数
+    """
+    pass
+
+if __name__ == '__main__':
+    test()
+else:
+    pass
